33b7537a969ee6d0e6a26ae4ab83d8efe4193951
tngh4037 <tngh4037@gmail.com>
2026-04-23 13:15:03 +0900

    init

---
+++ src/test/java/com/batch/settlement/SettlementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.batch.settlement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SettlementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
